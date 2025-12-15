6dfece691c396075eec5fa994253277f2ac4f293
Wholesome-Student <E3ee4vxnk0a@outlook.jp>
2025-12-15 19:33:12 +0900

    👍 choiceに値を代入した

--- database.sql
+++ database.sql
@@ -66,3 +66,10 @@ ALTER TABLE `user`
 
 ALTER USER 'kbtxdb'@'%' IDENTIFIED WITH mysql_native_password BY 'password';
 FLUSH PRIVILEGES;
+
+INSERT INTO `choice` (content) VALUES
+  ('A'), ('B'), ('C'), ('D'), ('E'),
+  ('F'), ('G'), ('H'), ('I'), ('J'),
+  ('K'), ('L'), ('M'), ('N'), ('O'),
+  ('P'), ('Q'), ('R'), ('S'), ('T'),
+  ('U'), ('V'), ('W'), ('Y'), ('Z');
